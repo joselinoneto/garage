ed318288e7668944557fbdfc84fa1f08568c845e
José Neto <joselinoneto@gmail.com>
2024-03-17 18:54:52 +0000

    Image support

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :description, :sold)
+      params.require(:product).permit(:title, :description, :sold, :thumbnail)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    has_one_attached :thumbnail do |attachable|
+        attachable.variant :thumb, resize_to_limit: [800, 600], preprocessed: true
+    end
 end
