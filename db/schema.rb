9fa316ae278a44a85e90c0acbd8b9673acaccf98
José Neto <joselinoneto@gmail.com>
2024-03-17 21:50:39 +0000

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_171337) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_210247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_171337) do
     t.string "title"
     t.text "description"
     t.boolean "sold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
