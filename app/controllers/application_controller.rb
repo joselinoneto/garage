21ad58936df7343204eb66f6c7fd912a42289e4d
José Neto <joselinoneto@gmail.com>
2024-03-18 20:28:51 +0000

    User access, design,

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization  
   helper_method :current_user
+  helper_method :current_user_admin
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def current_user
       # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
       @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
+  def current_user_admin
+    # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
+    if current_user
+      user = User.find_by(id: session[:user_id])
+      user.admin
+    end
   end
 
 private
